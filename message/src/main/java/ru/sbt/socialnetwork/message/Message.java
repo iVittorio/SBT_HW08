1642b14639bda1207d00f7a2b3172b1999e49cbb
Viktor Ivanov <i.viktor@yandex.ru>
2016-08-14 21:40:38 +0300

    in work

--- message/src/main/java/ru/sbt/socialnetwork/message/Message.java
+++ message/src/main/java/ru/sbt/socialnetwork/message/Message.java
@@ -1,10 +1,4 @@
 package ru.sbt.socialnetwork.message;
 
-/**
- * Created by chapaev on 14.08.2016.
- */
 public interface Message {
-    void sentMessage(String toUserId, String text);
-
-    String readMessage(String fromUserId);
 }
